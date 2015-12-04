d803825a6e59a272667fe7b091688c98cde7732f
Micah Koki <micah.koki@gmail.com>
2015-12-04 04:30:26 -0800

    Finished Largest Prime Factor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,31 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
-
+  var i = 2;
+  while (i <= n){
+    if (isDivisible(n, i)) {
+      if (isPrime(i)) {
+        console.log(i);
+        primeNumber = i;
+      }
+    }
+    i++;
+  }
   return primeNumber;
 };
+
+  function isDivisible(n, i){
+  if(n % i === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+  function isPrime(value){
+    for (i = 2; i < value; i++){
+      if (value % i === 0){
+        return false;
+      }
+    }
+    return value;
+}
